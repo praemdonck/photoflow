13fd2e800868b55d35ce54656c51395eeb1d6202
Pablo Raemdonck <praemdonck@gmail.com>
2016-10-09 21:42:37 +0100

    Added exception handler to data_to_name.py and time_correct.py

--- date_to_name.py
+++ date_to_name.py
@@ -5,17 +5,22 @@ import datetime
 from gi.repository import GExiv2
 import os
 
-image_list = glob.glob("IMG_9*")
+#image_list = glob.glob("IMG_9*")
+image_list = glob.glob("*.JPG")
 
 for i in image_list:
-  p = GExiv2.Metadata(i)
-  date_time = p.get_date_time()
-  name_string = date_time.strftime("%Y%m%d_%H%M%S")
-  b, e = os.path.splitext(i)
-  name_string += e
-  print name_string + " " + i
-  if not os.path.isfile(name_string):
-    os.rename(i, name_string)
-  else:
-    print "File %s already exists" % name_string
+  try:
+    p = GExiv2.Metadata(i)
+    date_time = p.get_date_time()
+    name_string = date_time.strftime("%Y%m%d_%H%M%S")
+    b, e = os.path.splitext(i)
+    e = ".JPG"
+    name_string += e
+    print name_string + " " + i
+    if not os.path.isfile(name_string):
+      os.rename(i, name_string)
+    else:
+      print "File %s already exists" % name_string
+  except:
+    pass
 
--- time_correct.py
+++ time_correct.py
@@ -4,15 +4,19 @@ import glob
 import datetime
 from gi.repository import GExiv2
 
-image_list = glob.glob("IMG_3*.*")
+#image_list = glob.glob("IMG_3*.*")
+image_list = glob.glob("*.JPG")
 #time_offset = datetime.timedelta(-1, 59274)
-time_offset = datetime.timedelta(-89, 12504)
+time_offset = datetime.timedelta(0, 10*3600)
 
 for i in image_list:
-  p = GExiv2.Metadata(i)
-  date_time = p.get_date_time()
-  corrected_time = date_time + time_offset
-  print i + " " + str(date_time) + " " + str(corrected_time)
-  p.set_date_time(corrected_time)
-  p.save_file(i)
+  try:
+    p = GExiv2.Metadata(i)
+    date_time = p.get_date_time()
+    corrected_time = date_time + time_offset
+    print i + " " + str(date_time) + " " + str(corrected_time)
+    p.set_date_time(corrected_time)
+    p.save_file(i)
+  except:
+    pass
   
